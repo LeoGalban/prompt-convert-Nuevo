3ea932a05d04b2c0e225ab859145b6546e3981d3
Leonel Galban <leonelgalban13@gmail.com>
2025-05-12 03:35:29 -0300

    Hice el menu con opciones de conversión, historial y salir

--- prompt-convert-Nuevo/app.js
+++ prompt-convert-Nuevo/app.js
@@ -12,3 +12,24 @@ function convertirARSDolares(montoARS) {
     return 0; // Si el monto es valido devuelve 0 //
 }
 
+//Bucle//
+do { 
+    //Menu de opciones//
+    seleccionUsuario = parseInt(prompt("1) Convertir ARS a USD\n2) Ver historial\n3) Salir"));
+
+    //Condicionales segun la opión selecionada//
+    if (seleccionUsuario === 1) {
+        //Si la opción del usuario es 1, Ingresa a la calculadora.
+
+        //Prompt para ingresar el monto en ARS a convertir a USD" //
+        montoARS = parseFloat(prompt("Introduce el monto en ARS para convertir a USD:"));
+        
+        if (montoARS >0 ) { //Verifica que el monto sea positivo//
+            let resultadoUSD = convertirARSDolares(montoARS);
+            alert("El monto es USD es: " + resultadoUSD.toFixed(2) + "USD");
+
+            
+
+        }
+    }
+};
